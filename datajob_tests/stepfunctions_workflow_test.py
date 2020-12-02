a3858d2d825b80bd1a3dfd4c38eaf3d0720a702b
vincent <vclaes1986@gmail.com>
2020-12-02 16:56:45 +0100

    fix step functions tests

---
+++ datajob_tests/stepfunctions_workflow_test.py
@@ -0,0 +1,60 @@
+import unittest
+
+from moto import mock_stepfunctions
+from stepfunctions.steps.compute import GlueStartJobRunStep
+from stepfunctions.steps.states import Parallel
+
+from datajob import stepfunctions_workflow
+from datajob.stepfunctions_workflow import StepfunctionsWorkflow
+
+
+@stepfunctions_workflow.task
+class SomeMockedClass(object):
+    def __init__(self, unique_name):
+        self.unique_name = unique_name
+
+
+class StepfunctionsWorkflowTestTest(unittest.TestCase):
+    @mock_stepfunctions
+    def test_create_tasks_for_orchestration_simple_flow_successfully(self):
+        task1 = stepfunctions_workflow.task(SomeMockedClass("task1"))
+        task2 = stepfunctions_workflow.task(SomeMockedClass("task2"))
+        task3 = stepfunctions_workflow.task(SomeMockedClass("task3"))
+        task4 = stepfunctions_workflow.task(SomeMockedClass("task4"))
+
+        with StepfunctionsWorkflow(
+                "some-name",
+                "arn:aws:iam::303915887687:role/some-role"
+        ) as a_step_functions_workflow:
+            task1 >> [task2, task3] >> task4
+
+        self.assertTrue(
+            isinstance(a_step_functions_workflow.chain_of_tasks[0], GlueStartJobRunStep)
+        )
+        self.assertTrue(
+            isinstance(a_step_functions_workflow.chain_of_tasks[1], Parallel)
+        )
+        self.assertTrue(
+            isinstance(a_step_functions_workflow.chain_of_tasks[2], GlueStartJobRunStep)
+        )
+
+    @mock_stepfunctions
+    def test_create_tasks_for_orchestration_starts_with_parallel_flow_successfully(
+            self,
+    ):
+        task1 = stepfunctions_workflow.task(SomeMockedClass("task1"))
+        task2 = stepfunctions_workflow.task(SomeMockedClass("task2"))
+        task3 = stepfunctions_workflow.task(SomeMockedClass("task2"))
+
+        with StepfunctionsWorkflow(
+                "some-name",
+                "arn:aws:iam::303915887687:role/some-role"
+        ) as a_step_functions_workflow:
+            [task1, task2] >> task3
+
+        self.assertTrue(
+            isinstance(a_step_functions_workflow.chain_of_tasks[0], Parallel)
+        )
+        self.assertTrue(
+            isinstance(a_step_functions_workflow.chain_of_tasks[1], GlueStartJobRunStep)
+        )
